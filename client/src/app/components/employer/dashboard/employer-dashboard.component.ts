07a06bf4cbb2b8974922a419f2cbbc5ee0d6ec2d
AdiGod14 <aditya.gode@gmail.com>
2025-06-05 17:06:35 +0530

    login and dashboard css

--- client/src/app/components/employer/dashboard/employer-dashboard.component.ts
+++ client/src/app/components/employer/dashboard/employer-dashboard.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employer-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employer-dashboard.component.html',
   styleUrls: ['./employer-dashboard.component.css']
 })
@@ -13,8 +14,72 @@ export class EmployerDashboardComponent implements AfterViewInit {
   employer: any = null;
   jobListings: any[] = [];
   employerId: string | null =null;
-
+  newJob: any = {
+    title: '',
+    domain: '',
+    description: {
+      overview: '',
+      responsibilities: [],
+      requiredSkills: [],
+      preferredSkills: [],
+      whatWeOffer: []
+    },
+    company: '',
+    location: '',
+    salary: '',
+    type: '',
+    experience: '',
+    vacancies: '',
+    status: 'open'
+  };
   constructor(private http: HttpClient) {}
+
+  addJob() {
+    if (!this.employerId || !this.employer) {
+      console.warn('Employer not loaded or invalid');
+      return;
+    }
+
+    const payload = {
+      ...this.newJob,
+      employerId: this.employerId,
+      employerName: this.employer.name,
+    };
+
+    this.http.post('http://localhost:3000/api/employer/addJob', payload).subscribe({
+      next: () => {
+        alert('Job added successfully!');
+        this.loadJobListings(); // Refresh the list
+        this.resetForm();
+      },
+      error: (err) => {
+        console.error('Failed to add job:', err);
+        alert('Failed to add job');
+      }
+    });
+  }
+   resetForm() {
+    this.newJob = {
+      title: '',
+      domain: '',
+      description: {
+        overview: '',
+        responsibilities: [],
+        requiredSkills: [],
+        preferredSkills: [],
+        whatWeOffer: []
+      },
+      company: '',
+      location: '',
+      salary: '',
+      type: '',
+      experience: '',
+      vacancies: '',
+      status: 'open'
+    };
+  }
+
+  
 
 
   ngAfterViewInit() {
