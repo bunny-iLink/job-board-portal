df890a46f79c73d2f535b6607c8f7930c88287e9
AdiGod14 <aditya.gode@gmail.com>
2025-06-10 11:50:38 +0530

    minor changes

---
+++ client/src/app/components/user/applied-jobs/applied-jobs.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applied-jobs',
+  imports: [],
+  templateUrl: './applied-jobs.html',
+  styleUrl: './applied-jobs.css'
+})
+export class AppliedJobs {
+
+}
