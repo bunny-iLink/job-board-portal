fd6eb115d8bc1ab01224bbcb864ecfbe8ff9db29
bunny-iLink <ankush.kaudi@ilink-systems.com>
2025-06-11 10:37:34 +0530

    Add Job modal fixes

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://job-board-portal.onrender.com'
+  // apiUrl: 'https://job-board-portal.onrender.com'
+  apiUrl: 'http://localhost:3000'
 };
