61bd86675f6f29d9633c53fecd42cb3c3b5a171a
bunny-iLink <ankush.kaudi@ilink-systems.com>
2025-07-04 10:39:14 +0530

    minor UI changes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes, Route } from '@angular/router';
 import { authGuard } from './auth/auth-guard';
 // Import the eagerly loaded EmployerLayoutComponent since we need to use it as `component` (not lazy-loaded)
-import { EmployerLayoutComponent } from './components/employer/employer-layout/employer-layout';
+import { EmployerLayoutComponent } from './components/employer-specific-components/employer-layout/employer-layout';
 
 export const routes: Routes = [
   {
@@ -34,27 +34,27 @@ export const routes: Routes = [
         path: 'dashboard',
         loadComponent: () =>
           import(
-            './components/employer/dashboard/employer-dashboard.component'
+            './components/employer-specific-components/dashboard/employer-dashboard.component'
           ).then((m) => m.EmployerDashboardComponent),
       },
       {
         path: 'profile',
         loadComponent: () =>
           import(
-            './components/employer/employer-profile/employer-profile'
+            './components/employer-specific-components/employer-profile/employer-profile'
           ).then((m) => m.EmployerProfileComponent),
       },
       {
         path: 'my-listings',
         loadComponent: () =>
-          import('./components/employer/my-listings/my-listings').then(
+          import('./components/employer-specific-components/my-listings/my-listings').then(
             (m) => m.MyListingsComponent
           ),
       },
       {
         path: 'job-details',
         loadComponent: () =>
-          import('../app/components/employer/job-details/job-details').then(
+          import('./components/employer-specific-components/job-details/job-details').then(
             (m) => m.JobDetailsComponent
           ),
         // renderMode: 'direct'
@@ -65,7 +65,7 @@ export const routes: Routes = [
   {
     path: 'user',
     loadComponent: () =>
-      import('./components/user/user-layout/user-layout').then(
+      import('./components/user-specific-components/user-layout/user-layout').then(
         (m) => m.UserLayoutComponent
       ),
     canActivate: [authGuard],
@@ -74,28 +74,28 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         loadComponent: () =>
-          import('./components/user/dashboard/user-dashboard.component').then(
+          import('./components/user-specific-components/dashboard/user-dashboard.component').then(
             (m) => m.UserDashboardComponent
           ),
       },
       {
         path: 'profile',
         loadComponent: () =>
-          import('./components/user/user-profile/user-profile').then(
+          import('./components/user-specific-components/user-profile/user-profile').then(
             (m) => m.UserProfileComponent
           ),
       },
       {
         path: 'jobs',
         loadComponent: () =>
-          import('./components/user/search-jobs/search-jobs').then(
+          import('./components/user-specific-components/search-jobs/search-jobs').then(
             (m) => m.SearchJobsComponent
           ),
       },
       {
         path: 'applied-jobs',
         loadComponent: () =>
-          import('./components/user/applied-jobs/applied-jobs').then(
+          import('./components/user-specific-components/applied-jobs/applied-jobs').then(
             (m) => m.AppliedJobsComponent
           ),
       },
